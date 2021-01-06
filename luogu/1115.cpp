602f567a7c459438d10390d9bb4d66378545cb15
hsingyang <hsingyang@126.com>
2021-01-06 18:10:26 +0800

    更新 洛谷1115、5638

--- luogu/1115.cpp
+++ luogu/1115.cpp
@@ -1,26 +1,38 @@
 // https://www.luogu.com.cn/problem/P1115 P1115 最大子段和
+// 以前缀和为模板的动态规划
 
-#include <iostream>
 #include <algorithm>
+#include <cstdio>
 
 using namespace std;
 
 //const int maxN = 2e5 + 10;
 const int INF = 2e9 + 1;
 int a;
-int s[2] = {0};  //前缀和
+// int s[maxN] = {0};//前缀和
+int s[2] = {0};//前缀和
 
 int main() {
-    ios::sync_with_stdio(false);
     int n;
-    cin >> n;
+    scanf("%d", &n);
     int ans = -INF;
     for (int i = 1; i <= n; i++) {
-        cin >> a;
+        scanf("%d", &a);
+        // if(i<2) s[i] = a;
         if (i < 2) s[1] = a;
-        else s[i&1] = max(a, s[!(i&1)] + a);    //滚动数组
-        ans = max(ans, s[i&1]);
+        else
+            // 比较当前输入的数a和<上一次字段和加上a><s[i+1]+a>哪个大
+            // 如果a本身就比s[i+1]+a大,就没有将s[i+1]+a作为新的字段和s[i]的必要
+
+            // s[i] = max(a, s[i-1]+a));
+            s[i & 1] = max(a, s[!(i & 1)] + a);//滚动数组
+            // 实际上只要保留两个关键的值:新字段和和上一次字段和
+
+        // ans用于记录最大字段和
+        // 由于存在<a本身就比s[i+1]+a大>的情况,会有不同的字段和
+        // ans = max(ans, s[i]);
+        ans = max(ans, s[i & 1]);
     }
-    cout << ans;
+    printf("%d\n", ans);
     return 0;
 }
--- luogu/5638.cpp
+++ luogu/5638.cpp
@@ -1,38 +1,37 @@
 // https://www.luogu.com.cn/problem/P5638 【CSGRound2】光骓者的荣耀
+// 前缀和、模拟
 
-#include <iostream>
 #include <algorithm>
+#include <cstdio>
 
 using namespace std;
 
 const int maxN = (int) 1e6 + 5;
 
 //long long a[maxN] = {0}, s[maxN] = {0};
-long long a = 0, s[maxN] = {0}; // a用不到数组，直接变为变量
+long long a = 0, s[maxN] = {0};// a用不到数组，直接变为变量
 
 int main() {
-    ios::sync_with_stdio(false);
     int n, k;
-    cin >> n >> k;
+    scanf("%d %d", &n, &k);
     for (int i = 1; i <= n - 1; i++) {
-//        cin >> a[i];
-//        s[i] = s[i - 1] + a[i]; // 前缀和
-        cin >> a;
-        s[i] = s[i - 1] + a; // 前缀和
+        scanf("%lld", &a);
+        s[i] = s[i - 1] + a;// 前缀和
     }
     if (!k)
-        cout << s[n - 1] << endl;
-    else if (k >= n) cout << 0 << endl;
+        printf("%lld\n", s[n - 1]);
+    else if (k >= n)
+        printf("0\n");
     else {
-        long long ans = 0;
-        // 枚举出每一个城市作为跳跃出发点
+        long long skip = 0;
+        // 枚举出每一个城市作为跳跃出发点，skip是计算跳过的路径
         for (int i = k; i <= n - 1; i++) {
-            ans = max(ans, s[i] - s[i - k]);
+            skip = max(skip, s[i] - s[i - k]);
         }
-//        for (int i = 1; i <= n - 1; i++) {
-//            ans = max(ans, s[i + k - 1] - s[i - 1]);
-//        }
-        cout << s[n - 1] - ans << endl;
+        // for (int i = 1; i <= n - k; i++) {
+        //     skip = max(skip, s[i + k - 1] - s[i - 1]);
+        // }
+        printf("%lld\n", s[n - 1] - skip);
     }
     return 0;
 }
