2511264821ecfd5ab80cb8631def742ca6ca54dc
hsingyang <hsingyang@126.com>
2021-01-10 16:43:28 +0800

    更新 01-前缀和.cpp

--- AlgorithmExample/01-前缀和.cpp
+++ AlgorithmExample/01-前缀和.cpp
@@ -7,11 +7,12 @@ int main() {
     int n;
     cout << "输入数组的大小:";
     cin >> n;
-    int a[n + 1] = {0};
+    int a[n + 1] = {0};//原始数组
     int b[n + 1] = {0};//前缀和
     cout << "请输入这" << n << "个数" << endl;
+    // todo: 1.预处理
     for (int i = 1; i <= n; i++) {
-        cin >> a[i];
+        cin >> a[i];// 实际用不到数组,可更改为a,节省空间
         b[i] = b[i - 1] + a[i];
     }
     int q;
@@ -25,6 +26,7 @@ int main() {
         cin >> l;
         cout << "请输入右区间r:";
         cin >> r;
+        // todo: 2.计算
         cout << "区间[" << l << "," << r << "]和为:" << b[r] - b[l - 1] << endl;
     }
 }
